acf5e173c83cf38fcb3f92656f681ce0d59d9526
Geremia Taglialatela <tagliala.dev@gmail.com>
2018-09-22 14:18:48 +0200

    Add GEOS 3.7.0 and PROJ.4 5.2.0

---
+++ scripts/libraries/geos_3.7.x.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+LIBRARY_VERSION=3.7.0
+
+curl -O http://download.osgeo.org/geos/geos-${LIBRARY_VERSION}.tar.bz2 \
+  && tar -xjvf geos-${LIBRARY_VERSION}.tar.bz2 \
+  && cd geos-${LIBRARY_VERSION} \
+  && ./configure --prefix=${HEROKU_VENDOR_DIR} \
+  && make && make install \
+  && tar -C ${HEROKU_VENDOR_DIR} -czvf ${TARGET_DIR}/geos-${LIBRARY_VERSION}-heroku.tar.gz .
---
+++ scripts/libraries/proj4_5.2.x.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+LIBRARY_VERSION=5.2.0
+
+curl -O http://download.osgeo.org/proj/proj-${LIBRARY_VERSION}.tar.gz \
+  && tar -xvf proj-${LIBRARY_VERSION}.tar.gz \
+  && cd proj-${LIBRARY_VERSION} \
+  && ./configure --prefix=${HEROKU_VENDOR_DIR} \
+  && make && make install \
+  && tar -C ${HEROKU_VENDOR_DIR} -czvf ${TARGET_DIR}/proj-${LIBRARY_VERSION}-heroku.tar.gz .
