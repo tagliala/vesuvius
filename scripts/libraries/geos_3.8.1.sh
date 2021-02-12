260415db6706e1fe5e9d8525cbb06267c5f1b557
Geremia Taglialatela <tagliala.dev@gmail.com>
2021-02-12 15:27:44 +0100

    Download over https

--- scripts/libraries/geos_3.8.1.sh
+++ scripts/libraries/geos_3.8.1.sh
@@ -2,7 +2,7 @@
 
 LIBRARY_VERSION=3.8.1
 
-curl -O http://download.osgeo.org/geos/geos-${LIBRARY_VERSION}.tar.bz2 \
+curl -O https://ftp.osuosl.org/pub/osgeo/download/geos/geos-${LIBRARY_VERSION}.tar.bz2 \
   && tar -xjvf geos-${LIBRARY_VERSION}.tar.bz2 \
   && cd geos-${LIBRARY_VERSION} \
   && ./configure --prefix=${HEROKU_VENDOR_DIR} \
--- scripts/libraries/proj_4.9.3.sh
+++ scripts/libraries/proj_4.9.3.sh
@@ -2,7 +2,7 @@
 
 LIBRARY_VERSION=4.9.3
 
-curl -O http://download.osgeo.org/proj/proj-${LIBRARY_VERSION}.tar.gz \
+curl -O https://ftp.osuosl.org/pub/osgeo/download/proj/proj-${LIBRARY_VERSION}.tar.gz \
   && tar -xvf proj-${LIBRARY_VERSION}.tar.gz \
   && cd proj-${LIBRARY_VERSION} \
   && ./configure --prefix=${HEROKU_VENDOR_DIR} \
