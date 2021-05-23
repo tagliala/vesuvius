12b8bf3fcc1e236dd551b53e41f03f057347a9e1
Geremia Taglialatela <tagliala.dev@gmail.com>
2021-05-23 15:28:18 +0200

    Add MD5 checksums

--- scripts/libraries/test.sh
+++ scripts/libraries/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 LIBRARY_VERSION=0.1.0
+OUTPUT_LIBRARY_NAME="vesuvius-${LIBRARY_VERSION}-heroku.tar.gz"
 
 echo "Vesuvius ${LIBRARY_VERSION} Test Script" > ${HEROKU_VENDOR_DIR}/vesuvius.txt \
-  && tar -C ${HEROKU_VENDOR_DIR} -czvf ${TARGET_DIR}/vesuvius-${LIBRARY_VERSION}-heroku.tar.gz .
+  && tar -C ${HEROKU_VENDOR_DIR} -czvf ${TARGET_DIR}/${OUTPUT_LIBRARY_NAME} . \
+  && (cd ${TARGET_DIR} && exec md5sum ${OUTPUT_LIBRARY_NAME} > ${OUTPUT_LIBRARY_NAME}.md5)
